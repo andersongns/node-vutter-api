349aff2ef678ea06a612ca621a079e564a5dae37
Anderson Santos <anderson.santos@tuta.io>
2021-03-16 18:41:11 -0300

    chore: coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,11 @@
 module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb'
+  preset: '@shelf/jest-mongodb',
+  coverageThreshold: {
+    branches: 90,
+    functions: 90,
+    lines: 90,
+    statements: -10
+  }
 }
